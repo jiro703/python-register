b1b60ce28ea7f5e51dc8afc75aaf0caa89122389
Jiro703 <alexshot09@gmail.com>
2025-01-07 21:20:47 -0300

    hola

--- firts test.py
+++ firts test.py
@@ -1,3 +1,21 @@
 from os import system
 system("cls")
-print("hola mundo")
+
+
+a= input("pertecene a la florida?(si/no): ")
+if a == "si":
+    print("continua a la siguiente")
+    b = input("tiene carnet?(si/no): ")
+    if b == 'si':
+        carnet=int(input(("ingrese numero: ")))
+    if b == 'no':
+        print("crear carnet aqui!")
+        carnet=int(input("colocar 8 numeros: "))
+    c = input("es jubilado?(si/no): ")
+    if c == 'si':
+        monto=int(input("ingrese monto: "))*0.75
+        print("+ 25% -> total = ",round(monto))
+    if c == 'no':
+        print("se pago monto total $2000")    
+else:
+    print("gracias por visitar")
